8d4743106831c9e1bfed77659e624f24958f4b36
kotaro1993 <kotaro.s.1993@gmail.com>
2020-05-27 23:01:18 +0900

    StepAsideUnityChan　初回コミット
    初回コミットです

---
+++ Assets/ItemEraser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemEraser : MonoBehaviour {
+
+    Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.z < cam.transform.position.z - 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
